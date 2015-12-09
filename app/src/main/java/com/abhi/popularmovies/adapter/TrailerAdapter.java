234b212b08f37f5b974d3b5fb1a50d0e96818861
Kaushal <212424869@logon.ds.ge.com>
2015-12-09 19:31:50 +0530

    committing the Popular Movie 1st version

---
+++ app/src/main/java/com/abhi/popularmovies/adapter/TrailerAdapter.java
@@ -0,0 +1,37 @@
+package com.abhi.popularmovies.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.abhi.popularmovies.R;
+
+/**
+ * Class to create list of trailer
+ */
+public class TrailerAdapter extends ArrayAdapter<String> {
+
+
+    private Context mContext;
+    private String[] mTrailerArray;
+
+    public TrailerAdapter(Context context, String[] objects) {
+        super(context, 0, objects);
+        mContext = context;
+        mTrailerArray = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.trailer_row, parent, false);
+        }
+        TextView trailerName = (TextView) view.findViewById(R.id.trailer_name);
+        trailerName.setText(mTrailerArray[position]);
+        return view;
+    }
+}
